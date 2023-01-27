29c6e1f658dc93cb879cd2c38f9193944e25129d
Dhanraj Patil <dhanraj.patil@hotmail.com>
2023-01-27 11:14:26 +0530

    routes configured, Buttons and links updated

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -4,48 +4,48 @@ import { Product } from './product';
 import { ProductService } from './product.service';
 
 @Component({
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  pageTitle = 'Product List';
-  imageWidth = 50;
-  imageMargin = 2;
-  showImage = false;
-  errorMessage = '';
+    pageTitle = 'Product List';
+    imageWidth = 50;
+    imageMargin = 2;
+    showImage = false;
+    errorMessage = '';
 
-  _listFilter = '';
-  get listFilter(): string {
-    return this._listFilter;
-  }
-  set listFilter(value: string) {
-    this._listFilter = value;
-    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
-  }
+    _listFilter = '';
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
 
-  filteredProducts: Product[] = [];
-  products: Product[] = [];
+    filteredProducts: Product[] = [];
+    products: Product[] = [];
 
-  constructor(private productService: ProductService) { }
+    constructor(private productService: ProductService) { }
 
-  ngOnInit(): void {
-    this.productService.getProducts().subscribe({
-      next: products => {
-        this.products = products;
-        this.filteredProducts = this.performFilter(this.listFilter);
-      },
-      error: err => this.errorMessage = err
-    });
-  }
+    ngOnInit(): void {
+        this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.performFilter(this.listFilter);
+            },
+            error: err => this.errorMessage = err
+        });
+    }
 
-  performFilter(filterBy: string): Product[] {
-    filterBy = filterBy.toLocaleLowerCase();
-    return this.products.filter((product: Product) =>
-      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
-  }
+    performFilter(filterBy: string): Product[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: Product) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
 
-  toggleImage(): void {
-    this.showImage = !this.showImage;
-  }
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 
 }
